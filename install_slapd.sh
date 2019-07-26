cf40385db5e36aa642bdb51de0168d7807e2b29b
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-07-25 21:16:16 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+export DEBIAN_FRONTEND='non-interactive'
 
-heloo world
+echo -e "slapd slapd/root_password password KappaRoss" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password KappaRoss" |debconf-set-selections
+
+echo -e "slapd slapd/internal/adminpw password test" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password test" |debconf-set-selections
+echo -e "slapd slapd/password2 password test" |debconf-set-selections
+echo -e "slapd slapd/password1 password test" |debconf-set-selections
+echo -e "slapd slapd/domain string acu.local" |debconf-set-selections
+echo -e "slapd shared/organization string IT410" |debconf-set-selections
+echo -e "slapd slapd/backend string HDB" |debconf-set-selections
+echo -e "slapd slapd/purge_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
