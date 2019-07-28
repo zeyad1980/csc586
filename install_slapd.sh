51467020128ac4cd9a25de30201ed0187d35b3f9
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-07-28 13:51:04 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -16,5 +16,6 @@ echo -e "slapd slapd/purge_database boolean false" | debconf-set-selections
 echo -e "slapd slapd/move_old_database boolean true" | debconf-set-selections
 echo -e "slapd slapd/allow_ldap_v2 boolean false" | debconf-set-selections
 echo -e "slapd slapd/no_configuration boolean false" | debconf-set-selections
+apt install ldap-utils
 
 sudo dpkg-reconfigure slapd 
