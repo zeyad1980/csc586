a68f6e851465516289345a1c97331ae885c9a5b0
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-07-28 14:16:37 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -9,12 +9,12 @@ echo -e "slapd slapd/internal/generated_adminpw password admin" | debconf-set-se
 echo -e "slapd slapd/password2 password admin" | debconf-set-selections
 echo -e "slapd slapd/password1 password admin" | debconf-set-selections
 
-echo -e "slapd slapd/domain string clemson.cloudlab.us" | debconf-set-selections
-echo -e "slapd shared/organization string HH1234" | debconf-set-selections
-echo -e "slapd slapd/backend string MDB" | debconf-set-selections
-echo -e "slapd slapd/purge_database boolean false" | debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" | debconf-set-selections
-echo -e "slapd slapd/allow_ldap_v2 boolean false" | debconf-set-selections
-echo -e "slapd slapd/no_configuration boolean false" | debconf-set-selections
+#echo -e "slapd slapd/domain string clemson.cloudlab.us" | debconf-set-selections
+#echo -e "slapd shared/organization string HH1234" | debconf-set-selections
+#echo -e "slapd slapd/backend string MDB" | debconf-set-selections
+#echo -e "slapd slapd/purge_database boolean false" | debconf-set-selections
+#echo -e "slapd slapd/move_old_database boolean true" | debconf-set-selections
+#echo -e "slapd slapd/allow_ldap_v2 boolean false" | debconf-set-selections
+#echo -e "slapd slapd/no_configuration boolean false" | debconf-set-selections
 
 dpkg-reconfigure slapd 
