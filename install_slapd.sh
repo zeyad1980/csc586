ee3fbbd864f6dd16442e4eed551c32228add5a16
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-07-31 00:47:28 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
-export DEBIAN_FRONTEND='non-interactive'
+export DEBIAN_FRONTEND=noninteractive
 
+sudo apt update
 
-echo -e "slapd slapd/root_password password admin" | debconf-set-selections
-echo -e "slapd slapd/root_password_again password admin" | debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password admin" | debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password admin" | debconf-set-selections
-echo -e "slapd slapd/password2 password admin" | debconf-set-selections
-echo -e "slapd slapd/password1 password admin" | debconf-set-selections
+echo -e "slapd slapd/root_password password admin" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password_again password admin" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password admin" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password admin" |sudo debconf-set-selections
+echo -e "slapd slapd/password2 password admin" |sudo debconf-set-selections
+echo -e "slapd slapd/password1 password admin" |sudo debconf-set-selections
 
-echo -e "slapd slapd/domain string clemson.cloudlab.us" | debconf-set-selections
-echo -e "slapd shared/organization string HH1234" | debconf-set-selections
-echo -e "slapd slapd/backend string MDB" | debconf-set-selections
-echo -e "slapd slapd/purge_database boolean false" | debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" | debconf-set-selections
-echo -e "slapd slapd/allow_ldap_v2 boolean false" | debconf-set-selections
-echo -e "slapd slapd/no_configuration boolean false" | debconf-set-selections
+echo -e "slapd slapd/domain string clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd shared/organization string clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd slapd/backend string MDB" |sudo debconf-set-selections
+echo -e "slapd slapd/purge_database boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |sudo debconf-set-selections
 
-sudo dpkg-reconfigure slapd 
+sudo apt install -y slapd ldap-utils
+
+sudo ufw allow ldap
+
+PASS=$(slappasswd -s rammy)
+cat<<EOF >/local/repository/users.ldif
+dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+uid: student
+sn: Ram
+givenName: Golden
+cn: student
+displayName: student
+uidNumber: 10000
+gidNumber: 5000
+userPassword:"$PASS"
+gecos: Golden Ram
+loginShell: /bin/dash
+homeDirectory: /home/student
+EOF
+
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f basedn.ldif
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f users.ldif
+
