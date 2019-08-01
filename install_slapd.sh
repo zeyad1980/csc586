0d4f8f885378386f3670f4b9eb992e7afae709a0
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-08-01 14:22:41 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
 
 sudo apt update
 
-echo -e "slapd slapd/root_password password admin" |sudo debconf-set-selections
-echo -e "slapd slapd/root_password_again password admin" |sudo debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password admin" |sudo debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password admin" |sudo debconf-set-selections
-echo -e "slapd slapd/password2 password admin" |sudo debconf-set-selections
-echo -e "slapd slapd/password1 password admin" |sudo debconf-set-selections
+# Install openLDAP server quietly
+export DEBIAN_FRONTEND=noninteractive
 
-echo -e "slapd slapd/domain string clemson.cloudlab.us" |sudo debconf-set-selections
-echo -e "slapd shared/organization string clemson.cloudlab.us" |sudo debconf-set-selections
-echo -e "slapd slapd/backend string MDB" |sudo debconf-set-selections
-echo -e "slapd slapd/purge_database boolean false" |sudo debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
-echo -e "slapd slapd/allow_ldap_v2 boolean false" |sudo debconf-set-selections
-echo -e "slapd slapd/no_configuration boolean false" |sudo debconf-set-selections
+echo -e "
+slapd slapd/root_password password admin |sudo debconf-set-selections
+slapd slapd/root_password_again password admin |sudo debconf-set-selections
+slapd slapd/internal/adminpw password admin |sudo debconf-set-selections
+slapd slapd/internal/generated_adminpw password admin |sudo debconf-set-selections
+slapd slapd/password2 password admin |sudo debconf-set-selections
+slapd slapd/password1 password admin |sudo debconf-set-selections
+sapd slapd/domain string clemson.cloudlab.us |sudo debconf-set-selections
+slapd shared/organization string clemson.cloudlab.us |sudo debconf-set-selections
+slapd slapd/backend string MDB |sudo debconf-set-selections
+slapd slapd/purge_database boolean false |sudo debconf-set-selections
+slapd slapd/move_old_database boolean true |sudo debconf-set-selections
+slapd slapd/allow_ldap_v2 boolean false |sudo debconf-set-selections
+slapd slapd/no_configuration boolean false"
+|sudo debconf-set-selections
 
+# Grab slapd and ldap-utils (pre-seeded)
 sudo apt install -y slapd ldap-utils
 
+# Enable firewall rule
 sudo ufw allow ldap
 
+# Populate LDAP
 ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f basedn.ldif
 
+# Generate password hash
 PASS=$(slappasswd -s rammy)
 cat<<EOF >/local/repository/users.ldif
 dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
@@ -43,8 +50,9 @@ loginShell: /bin/dash
 homeDirectory: /home/student
 EOF
 
-
+# Populate LDAP
 ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f users.ldif
+
 # Test LDAP
 ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
 
