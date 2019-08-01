d24f7fa7c40c242f0fbd1573d3db700cf4e79866
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-07-31 22:47:10 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 sudo apt update
 
+#echo -e "ldap_auth_config        ldap_auth_config/ldapns/ldap-server     string  ldap://192.168.1.1" |sudo debconf-set-selections
+echo -e "ldap-auth-config        ldap-auth-config/rootbindpw password admin" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/bindpw password admin" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/rootbindpw password admin" |sudo debconf-set-selections
+echo -e "ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net" |sudo debconf-set-selections
+echo -e "ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true" |sudo debconf-set-selections
+echo -e "ldap-auth-config        ldap-auth-config/pam_password   select  md5" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/dbrootlogin    boolean true" |sudo debconf-set-selections
+echo -e "ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/ldapns/base-dn string  dc=clemson,dc=cloudlab,dc=us" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/rootbinddn     string  cn=admin,dc=clemson,dc=cloudlab,dc=us" |sudo debconf-set-selections
 echo -e "ldap_auth_config        ldap_auth_config/ldapns/ldap-server     string  ldap://192.168.1.1" |sudo debconf-set-selections
-echo -e "ldap_auth_config        ldap_auth_config/ldapns/ldap-server     string  ldap://192.168.1.1" |sudo debconf-set-selections
+echo -e "libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/ldapns/ldap_version    select  3" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/dblogin        boolean false" |sudo debconf-set-selections
+echo -e "ldap_auth_config        ldap_auth_config/override       boolean true"  |sudo debconf-set-selections
+
 
 clemson.cloudlab.us
 
