4a90ec6f15a3e6c0b5eda9fc5fb356559eefa0bf
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-08-01 23:32:32 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ADMIN_PASSWORD="admin"
+ADMIN_PASSWORD="zxc123"
 #updates the system repo database
 sudo apt update
 
 #change the fronend to noninteractive, avoiding prompts for automation
 export DEBIAN_FRONTEND=noninteractive
 echo "
-ldap_auth_config        ldap_auth_config/bindpw password admin
-ldap_auth_config        ldap_auth_config/rootbindpw password admin
+ldap_auth_config        ldap_auth_config/bindpw password zxc123
+ldap_auth_config        ldap_auth_config/rootbindpw password zxc123
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
 ldap-auth-config        ldap-auth-config/pam_password   select  md5
 ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
