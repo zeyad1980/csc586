fb25259e9d3f4d2d888177920103bd602fb8a616
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-08-01 23:05:04 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -25,17 +25,10 @@ sudo apt install -y libnss-ldap libpam-ldap ldap-utils
 
 #changes to ldap.conf, ldap.secret, nsswitch.conf, common-session, common-password
 
-# sudo sed -i 's/uri ldapi:\/\/\//uri ldap:\/\/192.168.1.1\//g' /etc/ldap.conf
-# sudo sed -i 's/base dc=example,dc=net/base dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
-# sudo sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=admin,dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
 sudo sed -i '/passwd:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/# end of pam-auth-update config/ i session optional pam_mkhomedir.so  skel=/etc/skel  umsak=077' /etc/pam.d/common-session
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
-# sudo bash <<EOF
-# echo $ADMIN_PASSWORD > /etc/ldap.secret
-# EOF
-# sudo chmod 600 /etc/ldap.secret
 
 #fetches and prints details for a particular user 
 getent passwd student
