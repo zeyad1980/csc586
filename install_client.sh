2d0be5147a59be6884946b1c285234a304789c9c
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-08-02 01:25:15 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+ADMIN_PASSWORD="admin"
 #updates the system repo database
 sudo apt update
 
 #change the fronend to noninteractive, avoiding prompts for automation
 export DEBIAN_FRONTEND=noninteractive
+
 echo "
 ldap_auth_config        ldap_auth_config/bindpw password admin
 ldap_auth_config        ldap_auth_config/rootbindpw password admin
@@ -16,17 +17,24 @@ ldap_auth_config        ldap_auth_config/rootbinddn     string  cn=admin,dc=clem
 ldap_auth_config        ldap_auth_config/ldapns/ldap-server     string  ldap://192.168.1.1
 ldap_auth_config        ldap_auth_config/ldapns/ldap_version    select  3
 ldap_auth_config        ldap_auth_config/dblogin        boolean false
-ldap_auth_config        ldap_auth_config/override       boolean true " |sudo debconf-get-selections
+ldap_auth_config        ldap_auth_config/override       boolean true " | sudo debconf-get-selections
 
 #installs libnss-ldap libpam-ldap ldap-utils along with all their dependencies
 sudo apt install -y libnss-ldap libpam-ldap ldap-utils
 
 #changes to ldap.conf, ldap.secret, nsswitch.conf, common-session, common-password
 
+sudo sed -i 's/uri ldapi:\/\/\//uri ldap:\/\/192.168.1.1\//g' /etc/ldap.conf
+sudo sed -i 's/base dc=example,dc=net/base dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
+sudo sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=admin,dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
 sudo sed -i '/passwd:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/# end of pam-auth-update config/ i session optional pam_mkhomedir.so  skel=/etc/skel  umsak=077' /etc/pam.d/common-session
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
+sudo bash <<EOF
+echo $ADMIN_PASSWORD > /etc/ldap.secret
+EOF
+sudo chmod 600 /etc/ldap.secret
 
 #fetches and prints details for a particular user 
 getent passwd student
