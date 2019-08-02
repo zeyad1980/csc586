e57df0234a7b449604c1501999ebc87baa8e395d
Zeyad Nidhamaldeen <53063574+zeyad1980@users.noreply.github.com>
2019-08-02 00:38:42 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -7,8 +7,8 @@ sudo apt update
 #change the fronend to noninteractive, avoiding prompts for automation
 export DEBIAN_FRONTEND=noninteractive
 echo "
-ldap_auth_config        ldap_auth_config/bindpw password zxc123
-ldap_auth_config        ldap_auth_config/rootbindpw password zxc123
+ldap_auth_config        ldap_auth_config/bindpw password admin
+ldap_auth_config        ldap_auth_config/rootbindpw password admin
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
 ldap-auth-config        ldap-auth-config/pam_password   select  md5
 ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
